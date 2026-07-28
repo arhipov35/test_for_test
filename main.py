a23c578ba55e362bbe33fba5bef4fe73dd777082
Vadym Arkhypov <arhipov1228603@gmail.com>
2026-07-28 21:03:48 +0300

    five her commit

--- main.py
+++ main.py
@@ -6,4 +6,4 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {1222222222222222222222222222222}')  # Press Ctrl+F8 to toggle the breakpoint.
